635ce114425f188105e8018c1044afa230a723fc
Novelty1 <novelty@rug.mail>
2020-10-16 13:07:09 +0200

    updaetz

--- src/main.cc
+++ src/main.cc
@@ -1,46 +1,62 @@
 #include <iostream>
-#include <sstream>
-
-#include <thread>
 
 #include "ros/ros.h"
 #include "ros/console.h"
 #include "std_msgs/String.h"
+#include "sensor_msgs/PointCloud2.h"
+#include "pcl-1.9/pcl/point_cloud.h"
+#include "pcl-1.9/pcl/point_types.h"
+#include "pcl-1.9/pcl/conversions.h"
+#include "pcl_ros/transforms.h"
+#include "pcl_ros/publisher.h"
+#include "pcl_conversions/pcl_conversions.h"
+#include "geometry_msgs/TransformStamped.h"
+#include "tf/transform_listener.h"
 
 using namespace std;
+using PointCloud = pcl::PointCloud<pcl::PointXYZ>;
+
+ros::Publisher pub;
+ros::Publisher utf;
+
+class Thingamajig
+{
+    tf::TransformListener d_tflistener;
+    PointCloud d_master_cloud;
+
+    public:
+        void the_callback(sensor_msgs::PointCloud2 const &cloud)
+        {
+            sensor_msgs::PointCloud2 post_tf;
+            geometry_msgs::TransformStamped transform;
+
+            // auto newcloud = ros::topic::waitForMessage<sensor_msgs::PointCloud2>("/xtion/depth_registered/points", ros::Duration(1.0));
+            d_tflistener.waitForTransform("/base_link", cloud.header.frame_id, ros::Time(0), ros::Duration(1.0));
+            pcl_ros::transformPointCloud("/base_link", cloud, post_tf, d_tflistener);
+            
+            pcl::PCLPointCloud2 pcl_pc2;
+            pcl_conversions::toPCL(post_tf, pcl_pc2);
+
+            PointCloud transformed;
+            pcl::fromPCLPointCloud2(pcl_pc2, transformed);
+
+            d_master_cloud += transformed;
+            pcl::toPCLPointCloud2(d_master_cloud, pcl_pc2);
+            pcl_conversions::fromPCL(pcl_pc2, post_tf);
+
+            pub.publish(post_tf);
+        }
+};
 
 int main(int argc, char *argv[])
 {
-    cout << "hello world!\n";
-
     ros::init(argc, argv, "pointcloud_poc");
-
     ros::NodeHandle nodehandle;
-    ros::Publisher outputter = nodehandle.advertise<std_msgs::String>("pcpoc_text", 1);
-    ros::Duration rate(1.0 / 10);
+    Thingamajig jig;
+    pub = nodehandle.advertise<sensor_msgs::PointCloud2>("pointcloud_plc_out", 1);
+    
+    // utf = nodehandle.advertise<sensor_msgs::PointCloud2>("pointcloud_plc_utf");
 
-    if (!outputter)
-    {
-        cerr << "outputter creation failed\n";
-        exit(-1);
-    }
-
-    size_t idx = 0;
-
-    auto callback = [&]([[maybe_unused]] ros::TimerEvent const& timer_event)
-    {
-        std_msgs::String msg;
-        stringstream ss;
-        ss << "hello #" << idx << "\n";
-        msg.data = ss.str();
-
-        ROS_INFO("%s", msg.data.c_str());
-        outputter.publish(msg);
-        ++idx;
-    };  
-
-    ros::Timer timer = nodehandle.createTimer(ros::Duration(0.1), callback);
+    ros::Subscriber depth_receiver = nodehandle.subscribe("/xtion/depth_registered/points", 5, &Thingamajig::the_callback, &jig);
     ros::spin();
-
-    return 0;
 }
